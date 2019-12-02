e8a778024f9699981357c799267167e0b8fbe5ce
wellingtonpecchini <wellingtonpecchini@gmail.com>
2019-12-02 09:02:16 +0100

    create infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef, useCallback } from 'react';
 import './App.css';
+import useBookSearch from './useBookSearch'
 
 function App() {
+  const [query, setQuery] = useState('')
+  const [pageNumber, setPageNumber] = useState(1)
+  const {
+    books,
+    hasMore,
+    loading,
+    error
+  } = useBookSearch(query, pageNumber);
+
+  // TODO capire la parte di USEREF
+  const observer = useRef();
+  // TODO capire la parte di CALLBACK
+  const lastBookElementRef = useCallback(node => {
+    if (loading) return
+    if (observer.current) observer.current.disconnect();
+
+    observer.current = new IntersectionObserver(entries => {
+
+      if (entries[0].isIntersecting && hasMore) {
+        console.log('entries', entries)
+        setPageNumber(prevPageNumber => prevPageNumber + 1)
+      }
+    });
+    if (node) {
+
+      observer.current.observe(node);
+    }
+  }, [hasMore, loading])
+
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
+    setPageNumber(1)
+    // clearInterval(int)
+    // setTime(time)
+  }
+  // var int = setInterval(function () { window.location.reload(); }, time);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <input type="text" onChange={handleSearch}></input>
+      {books.map((book, index) => {
+        if (books.length === index + 1) {
+          return <div ref={lastBookElementRef} key={book}>{book}</div>
+        }
+        else {
+          return <div ref={lastBookElementRef} key={book}>{book}</div>
+        }
+      })}
+      <div>{loading && "Loading..."}</div>
+      <div>{error && "Error..."}</div>
     </div>
   );
 }
---
+++ src/useBookSearch.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function useBoolSearch(query, pageNumber) {
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+  const [books, setBooks] = useState([]);
+  const [hasMore, setHasMore] = useState(false)
+
+  useEffect(() => {
+    setBooks([])
+  }, [query])
+
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+    let cancel;
+    axios({
+      method: "GET",
+      url: 'http://openlibrary.org/search.json',
+      params: { q: query, page: pageNumber },
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      // TODO non ho capito la parte di SET
+      setBooks(prevBooks => { return [...new Set([...prevBooks, ...res.data.docs.map(b => b.title)])] })
+      setHasMore(res.data.docs.length > 0)
+      setLoading(false)
+
+    }).catch(e => {
+      if (axios.isCancel(e)) return;
+      setError(true)
+    })
+    return () => cancel()
+  }, [query, pageNumber])
+  return { loading, error, books, hasMore }
+}
